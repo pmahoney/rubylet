2c447c967a0c45aeaac49b5085817539d43accf9
Patrick Mahoney <patrick.mahoney@commongroundpublishing.com>
2012-12-29 09:51:28 -0600

    Change formatting/nesting; add :expire_after option during initialization.

--- rubylet/lib/rubylet/session.rb
+++ rubylet/lib/rubylet/session.rb
@@ -1,33 +1,44 @@
-class Rubylet::Session
+# A Rack compatible session object that forwards to an underlying
+# +javax.servlet.http.HttpSession+.
+module Rubylet
+  class Session
+    # @param [javax.servlet.http.HttpServletRequest] req
+    # @param [Hash] options
+    # @option options [Integer] :expire_after The max inactive interval, in seconds
+    def initialize(req, options={})
+      @javaSession = req.getSession
 
-  def initialize(req)
-    @javaSession = req.getSession
-  end
-  
-  def store(key, value)
-    @javaSession.setAttribute(key, value)
-  end
-  
-  alias_method :[]=, :store
-  
-  def fetch(key, default = nil)
-    @javaSession.getAttribute(key) || default
-  end
-  
-  alias_method :[], :fetch
-  
-  def delete(key)
-    @javaSession.removeAttribute(key)
-  end
-  
-  def clear
-    @javaSession.getAttributeNames.each {|key| delete(key) }
-  end
+      if @javaSession.isNew
+        if exp = options[:expire_after]
+          req.setMaxInactiveInterval exp.to_i
+        end
+      end
+    end
+    
+    def store(key, value)
+      @javaSession.setAttribute(key, value)
+    end
+    alias_method :[]=, :store
+    
+    def fetch(key, default = nil)
+      @javaSession.getAttribute(key) || default
+    end
+    alias_method :[], :fetch
+    
+    def delete(key)
+      @javaSession.removeAttribute(key)
+    end
+    
+    def clear
+      @javaSession.getAttributeNames.each {|key| delete(key) }
+    end
 
-  def to_hash
-    hash = {}
-    @javaSession.getAttributeNames.each {|key| hash[key] = @javaSession.getAttribute(key) }
-    hash
+    def to_hash
+      hash = {}
+      @javaSession.getAttributeNames.each do
+        |key| hash[key] = @javaSession.getAttribute(key)
+      end
+      hash
+    end
   end
-  
 end
