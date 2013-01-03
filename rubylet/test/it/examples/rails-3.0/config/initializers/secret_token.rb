439cff4f582f2cca9e528e3e87f6c24dcc9194f8
Patrick Mahoney <patrick.mahoney@commongroundpublishing.com>
2013-01-03 15:40:19 -0600

    Add Rails 3.0 example to integration tests.

---
+++ rubylet/test/it/examples/rails-3.0/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '046d402ac18a07b3183fa6184f9e3dbfae7b68364384808362f57fe6a384c07f6af56d50790703cc61f988cbdeac83296f9121d8a19966e3892d1f34439f1242'
