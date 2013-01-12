47e0daf7cbe60e03212d9e461e9c0508f092328a
Patrick Mahoney <pat@polycrystal.org>
2013-01-11 20:51:44 -0600

    Add rack-1.4.3 integration test.

---
+++ rubylet/test/it/rack_1_4_3_it.rb
@@ -0,0 +1,22 @@
+# Integration test with plain Rack 1.4.3
+
+require 'it_helper'
+
+class Rack_1_4_3_It < MiniTest::Unit::TestCase
+  include Rubylet::IntegrationTestCase
+  self.app_root = File.expand_path('../examples/rack-1.4.3', __FILE__)
+
+  def test_delayed
+    resp = get('tests/delayed')
+    assert_equal 200, resp.code.to_i
+    assert_equal "delayed!", resp.body
+  end
+
+  # this assumes the server sends asynchronously even while it
+  # synchronously waits for the whole response; lame
+  def test_stream
+    resp = get('tests/stream')
+    assert_equal 200, resp.code.to_i
+    assert_equal 'Hello, World', resp.body
+  end
+end
