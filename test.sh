61d40c4157a6ffa7fdb552884ee97a81da770ffe
Patrick Mahoney <pat@polycrystal.org>
2013-02-23 13:56:20 -0600

    Run integration tests one by one to (maybe) avoid oom on travis.

--- test.sh
+++ test.sh
@@ -9,7 +9,14 @@ run() {
 }
 
 run rubylet bundle install
-run rubylet jruby -G -S rake test:spec test:integration
+run rubylet jruby -G -S rake test:spec
+# run these one by one to avoid oom on travis
+pushd rubylet
+for test in test/it/*_it.rb; do
+    echo "=== $test" 
+    jruby -G -S rake test:integration TEST="$test" || exit 1
+done
+popd
 
 run rubylet-tasks bundle install
 run rubylet-tasks jruby -G -S rake test
