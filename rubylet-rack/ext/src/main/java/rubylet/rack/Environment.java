363ea35c79a0fa06f2eceac7dc96d67fafa1a1b8
Patrick Mahoney <pat@polycrystal.org>
2013-03-13 22:31:55 -0500

    Remove unused imports.

--- rubylet-rack/ext/src/main/java/rubylet/rack/Environment.java
+++ rubylet-rack/ext/src/main/java/rubylet/rack/Environment.java
@@ -3,14 +3,12 @@ package rubylet.rack;
 import static rubylet.rack.Rubylet.defineConstant;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
-import org.jruby.RubyHash;
 import org.jruby.RubyIO;
 import org.jruby.RubyModule;
 import org.jruby.RubyString;
