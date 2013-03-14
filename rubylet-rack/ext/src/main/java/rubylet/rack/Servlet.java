3497b3673f41a4a12ee2db4d3ff4823c8539693b
Patrick Mahoney <pat@polycrystal.org>
2013-03-14 08:09:01 -0500

    Move catch(:async) {} into the Java layer.

--- rubylet-rack/ext/src/main/java/rubylet/rack/Servlet.java
+++ rubylet-rack/ext/src/main/java/rubylet/rack/Servlet.java
@@ -12,12 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.jruby.Ruby;
 import org.jruby.RubyArray;
 import org.jruby.RubyClass;
+import org.jruby.RubyContinuation;
 import org.jruby.RubyHash;
 import org.jruby.RubyNumeric;
 import org.jruby.RubyObject;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
+import org.jruby.runtime.Arity;
 import org.jruby.runtime.Block;
+import org.jruby.runtime.BlockCallback;
+import org.jruby.runtime.CallBlock;
 import org.jruby.runtime.ObjectAllocator;
 import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.Visibility;
@@ -48,8 +52,8 @@ public final class Servlet extends RubyObject implements javax.servlet.Servlet {
     };
 
     private final RubyClass cAsyncCallback;
-    private final IRubyObject asyncThrown;
     private final IRubyObject asyncCallbackKey;
+    private final IRubyObject symAsync;
     
     private IRubyObject app;
     private ServletConfig servletConfig;
@@ -58,8 +62,8 @@ public final class Servlet extends RubyObject implements javax.servlet.Servlet {
         super(runtime, klass);
         
         cAsyncCallback = runtime.getModule("Rubylet").defineOrGetModuleUnder("Rack").getClass("AsyncCallback");
-        asyncThrown = klass.getConstant("ASYNC_THROWN");
         asyncCallbackKey = Rubylet.getConstant(runtime, "ASYNC_CALLBACK");
+        symAsync = runtime.newSymbol("async");
     }
     
     @JRubyMethod(optional = 1, visibility = Visibility.PRIVATE)
@@ -144,31 +148,55 @@ public final class Servlet extends RubyObject implements javax.servlet.Servlet {
         final HttpServletResponse resp = (HttpServletResponse) _resp;
 
         final Environment env = Environment.dupPrototype(runtime, req);
+
         if (req.getServletContext().getEffectiveMajorVersion() >= 3 && req.isAsyncSupported()) {
             env.put(asyncCallbackKey, new AsyncCallback(runtime, cAsyncCallback, req));
         }
 
-        // Servlet#call is implemented in ruby to allow catching :async.  It returns
-        // the async sentinel object if the app threw :async.  The app may also return
-        // a status of (-1) to start async.
-        final IRubyObject response = callMethod("call", app, env);
-        
-        if (response == asyncThrown) {
-            getAsyncCallback(context, env).ensureStarted();
-        } else {
-            // response is a Rack response [status, headers, body]
-            final RubyArray ary = response.convertToArray();
+        final Block block = CallBlock.newCallClosure(this, getMetaClass(), Arity.NO_ARGUMENTS, new BlockCallback() {
+
+            @Override
+            public IRubyObject call(ThreadContext context, IRubyObject[] _args, Block _block) {
+                return app.callMethod(context, "call", env);
+            }
             
-            final int status = RubyNumeric.fix2int(ary.entry(0));
-            if (status == -1) {
+        }, context);
+
+        /*
+         * catch(:async) do
+         *   # block body
+         * end
+         * 
+         * If :async is not thrown, returns the result of the block body.
+         * 
+         * If :async is thrown, returns nil?
+         */
+        final RubyContinuation rbContinuation = new RubyContinuation(runtime, symAsync);
+        try {
+            context.pushCatch(rbContinuation.getContinuation());
+
+            final IRubyObject response = rbContinuation.enter(context, symAsync, block);
+            if (response.isNil()) {
+                // async was thrown
                 getAsyncCallback(context, env).ensureStarted();
             } else {
-                final RubyHash headers = ary.entry(1).convertToHash();
-                final IRubyObject body = ary.entry(2);
-
-                final ResponseHelper wrappedResp = new ResponseHelper(resp, this, getMetaClass());
-                wrappedResp.respond(context, status, headers, body);
+                // response is a Rack response [status, headers, body]
+                final RubyArray ary = response.convertToArray();
+            
+                final int status = RubyNumeric.fix2int(ary.entry(0));
+                if (status == -1) {
+                    // alternate means of indicating async
+                    getAsyncCallback(context, env).ensureStarted();
+                } else {
+                    final RubyHash headers = ary.entry(1).convertToHash();
+                    final IRubyObject body = ary.entry(2);
+                    
+                    final ResponseHelper wrappedResp = new ResponseHelper(resp, this, getMetaClass());
+                    wrappedResp.respond(context, status, headers, body);
+                }
             }
+        } finally {
+            context.popCatch();
         }
     }
 
--- rubylet-rack/lib/rubylet/rack.rb
+++ rubylet-rack/lib/rubylet/rack.rb
@@ -3,16 +3,3 @@ require 'rubylet/rack/errors'
 require 'rubylet/rack/ext'
 require 'rubylet/rack/rewindable_io'
 require 'rubylet/rack/session/container_store'
-
-# Extension for Servlet since I don't know how to catch/throw on the
-# Java side.
-class Rubylet::Rack::Servlet
-  ASYNC_THROWN = Object.new.freeze
-
-  def call(app, env)
-    catch(:async) do
-      return app.call(env)
-    end
-    return ASYNC_THROWN
-  end
-end
