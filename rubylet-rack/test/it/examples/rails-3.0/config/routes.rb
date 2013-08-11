c8e9fc5c3d28926928f369a2bfe8f9945e34e04e
Patrick Mahoney <pat@polycrystal.org>
2013-08-11 13:17:04 -0500

    Remove Rails 3.0 integration test.

--- rubylet-rack/test/it/examples/rails-3.0/config/routes.rb
+++
@@ -1,6 +0,0 @@
-TestApp::Application.routes.draw do
-  match 'tests/log', :to => 'tests#log'
-  match 'tests/large/:size', :to => 'tests#large'
-  resources :session_values
-  root :to => 'tests#index'
-end
--- rubylet-rack/test/it/rails_3_0_it.rb
+++
@@ -1,8 +0,0 @@
-# Integration test with Rails 3.0
-
-require 'it_helper'
-
-class Rails_3_0_It < MiniTest::Unit::TestCase
-  include Rubylet::IntegrationTestCase
-  self.app_root = File.expand_path('../examples/rails-3.0', __FILE__)
-end
