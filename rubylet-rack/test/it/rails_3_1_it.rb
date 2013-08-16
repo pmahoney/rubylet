d00ca560ca2fd4cf25f32e39112ecfa5afcedeeb
Patrick Mahoney <pat@polycrystal.org>
2013-08-15 22:21:14 -0500

    Update for Minitest 5.

--- rubylet-rack/test/it/rails_3_1_it.rb
+++ rubylet-rack/test/it/rails_3_1_it.rb
@@ -2,7 +2,7 @@
 
 require 'it_helper'
 
-class Rails_3_1_It < MiniTest::Unit::TestCase
+class Rails_3_1_It < Minitest::Test
   include Rubylet::IntegrationTestCase
   self.app_root = File.expand_path('../examples/rails-3.1', __FILE__)
 end
