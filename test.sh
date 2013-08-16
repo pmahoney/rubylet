2cda19d5a889de4399c266bf3dfc277ff36fa4fb
Patrick Mahoney <pat@polycrystal.org>
2013-08-15 22:06:10 -0500

    Run integration tests together now that server runs in separate process.

--- test.sh
+++ test.sh
@@ -10,13 +10,7 @@ run() {
 
 run rubylet-rack bundle install
 run rubylet-rack jruby -G -S rake test:spec
-# run these one by one to avoid oom on travis
-pushd rubylet-rack
-for test in test/it/*_it.rb; do
-    echo "=== $test" 
-    jruby -G -S rake test:integration TEST="$test" || exit 1
-done
-popd
+run rubylet-rack jruby -G -S rake test:integration
 
 run rubylet-tasks bundle install
 run rubylet-tasks jruby -G -S rake test
